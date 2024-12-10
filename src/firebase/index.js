84c736b125ba8e9b8f98ae2048b2eaf1159e5ad1
Max Yang <max.yang342@gmail.com>
2024-12-10 13:54:20 -0500

    add firebase

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCLKQdyU-yprtObsU9rn5sk7shMNH-xX-Y",
+    authDomain: "summative-28d73.firebaseapp.com",
+    projectId: "summative-28d73",
+    storageBucket: "summative-28d73.firebasestorage.app",
+    messagingSenderId: "215705894364",
+    appId: "1:215705894364:web:16d5363cb55eba9187e319"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
